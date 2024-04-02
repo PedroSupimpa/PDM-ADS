ecf76aabeb401fb328634c8d58916cfbdc59e6d1
Pedro Souza <shiro2303@gmail.com>
2024-04-01 21:33:17 -0300

    upadate exercises class 03

--- aula03/exercises.dart
+++ aula03/exercises.dart
@@ -2,104 +2,104 @@
 
 void main() {
 
-// 1. Contagem
+// // 1. Contagem
 
-  for (int i = 1; i <= 10; i++) {
-    print(i);
-  }
-
-
-// 2. Soma Sequencial
-
-  print("Digite um número: ");
-  int n = 10
-
-  int soma = 0;
-  for (int i = 1; i <= n; i++) {
-    soma += i;
-  }
-
-  print("A soma dos números de 1 a $n é: $soma");
+//   for (int i = 1; i <= 10; i++) {
+//     print(i);
+//   }
 
 
-// 3. Lista de Números
+// // 2. Soma Sequencial
 
-  List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//   print("Digite um número: ");
+//   int n = 10
 
-  for (int numero in numeros) {
-    print(numero);
-  }
+//   int soma = 0;
+//   for (int i = 1; i <= n; i++) {
+//     soma += i;
+//   }
 
-
-// 4. Números Pares
-
-  for (int i = 2; i <= 50; i += 2) {
-    print(i);
-  }
+//   print("A soma dos números de 1 a $n é: $soma");
 
 
-// 5. While Contador
+// // 3. Lista de Números
 
-  int contador = 100;
-  while (contador >= 0) {
-    print(contador);
-    contador -= 5;
-  }
+//   List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//   for (int numero in numeros) {
+//     print(numero);
+//   }
 
 
-// 6. Inversão de Strings
+// // 4. Números Pares
 
-  print("Digite uma palavra: ");
-  String palavra = "Banana"
+//   for (int i = 2; i <= 50; i += 2) {
+//     print(i);
+//   }
+
+
+// // 5. While Contador
+
+//   int contador = 100;
+//   while (contador >= 0) {
+//     print(contador);
+//     contador -= 5;
+//   }
+
+
+// // 6. Inversão de Strings
+
+//   print("Digite uma palavra: ");
+//   String palavra = "Banana"
   
-  for (int i = palavra.length - 1; i >= 0; i--) {
-    print(palavra[i]);
-  }
+//   for (int i = palavra.length - 1; i >= 0; i--) {
+//     print(palavra[i]);
+//   }
 
 
-// 7. Tabela de Multiplicação
+// // 7. Tabela de Multiplicação
 
-  print("Digite um número: ");
-  int n = int.parse(stdin.readLineSync()!);
+//   print("Digite um número: ");
+//   int n = int.parse(stdin.readLineSync()!);
 
-  for (int i = 1; i <= 10; i++) {
-    print("$n x $i = ${n * i}");
-  }
+//   for (int i = 1; i <= 10; i++) {
+//     print("$n x $i = ${n * i}");
+//   }
 
 
-// 8. Lista até o Primeiro Negativo
+// // 8. Lista até o Primeiro Negativo
 
-  List<int> numeros = [];
-  int numero;
+//   List<int> numeros = [];
+//   int numero;
 
-  do {
-    print("Digite um número (negativo para parar): ");
-    numero = -5;
+//   do {
+//     print("Digite um número (negativo para parar): ");
+//     numero = -5;
     
-    if (numero >= 0) {
-      numeros.add(numero);
-    }
-  } while (numero >= 0);
+//     if (numero >= 0) {
+//       numeros.add(numero);
+//     }
+//   } while (numero >= 0);
 
-  print("Números positivos inseridos: $numeros");
+//   print("Números positivos inseridos: $numeros");
 
 
-// 9. Soma de Números Ímpares
+// // 9. Soma de Números Ímpares
 
-  print("Digite o número inicial: ");
-  int inicio = 5;
+//   print("Digite o número inicial: ");
+//   int inicio = 5;
 
-  print("Digite o número final: ");
-  int fim = 37;
+//   print("Digite o número final: ");
+//   int fim = 37;
 
-  int soma = 0;
-  for (int i = inicio; i <= fim; i++) {
-    if (i % 2 != 0) {
-      soma += i;
-    }
-  }
+//   int soma = 0;
+//   for (int i = inicio; i <= fim; i++) {
+//     if (i % 2 != 0) {
+//       soma += i;
+//     }
+//   }
 
-  print("A soma dos números ímpares entre $inicio e $fim é: $soma");
+//   print("A soma dos números ímpares entre $inicio e $fim é: $soma");
 
 
 // 10. Break e Continue
