ee9a520a742c1b87189cd8b72ae657d34ef38618
Pedro Souza <shiro2303@gmail.com>
2024-04-14 20:52:18 -0300

    Refactor main.dart to add a Todo List App and implement task management

--- flutter_project/flutter_application_1/lib/main.dart
+++ flutter_project/flutter_application_1/lib/main.dart
@@ -12,11 +12,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 12, 63, 230)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 12, 63, 230)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Todo List App'),
     );
   }
 }
@@ -30,41 +29,69 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Task {
+  final String title;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter += 3;
-    });
+  Task(this.title);
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Task> _tasks = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void _addTask() {
+    final String text = _controller.text;
+    if (text.isNotEmpty) {
+      setState(() {
+        _tasks.add(Task(text));
+        _controller.clear();
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'PLAY NOW FORTUNE TIGER!!!!',
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Add a new task',
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: _addTask,
+                ),
+              ),
+              onSubmitted: (_) => _addTask(),
             ),
-            Image.network(
-              'https://res.cloudinary.com/shotgun/image/upload/v1709994657/production/artworks/jogo_do_tigrinho_lzxayi.webp',
-              width: 500,
-              height: 500,
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                final task = _tasks[index];
+                return Dismissible(
+                  key: Key(task.title),
+                  onDismissed: (_) {
+                    setState(() {
+                      _tasks.removeAt(index);
+                    });
+                  },
+                  background: Container(color: Colors.red),
+                  child: ListTile(
+                    title: Text(task.title),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
