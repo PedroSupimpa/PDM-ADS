c9cde7d38dc6163e1798c1b7386609a6f937a701
Pedro Souza <shiro2303@gmail.com>
2024-04-15 20:48:47 -0300

    atividade rotas

---
+++ flutter_project/atividade_rotas/android/app/src/main/kotlin/com/example/atividade_rotas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.atividade_rotas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- flutter_project/flutter_application_1/lib/main.dart
+++ flutter_project/flutter_application_1/lib/main.dart
@@ -1,98 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Navigation with Data Passing',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch()
-            .copyWith(secondary: const Color(0xFF0C3FE6)),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Todo List App'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
+class HomePage extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class Task {
-  final String title;
-
-  Task(this.title);
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<Task> _tasks = [];
-  final TextEditingController _controller = TextEditingController();
-
-  void _addTask() {
-    final String text = _controller.text;
-    if (text.isNotEmpty) {
-      setState(() {
-        _tasks.add(Task(text));
-        _controller.clear();
-      });
-    }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    SecondPage(message: 'Hello, Second Page!'),
+              ),
+            );
+          },
+          child: Text('Go to Second Page'),
+        ),
+      ),
+    );
   }
+}
+
+class SecondPage extends StatelessWidget {
+  final String message;
+
+  SecondPage({required this.message});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Second Page'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                labelText: 'Add new task',
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.add),
-                  onPressed: _addTask,
-                ),
-              ),
-              onSubmitted: (_) => _addTask(),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _tasks.length,
-              itemBuilder: (context, index) {
-                final task = _tasks[index];
-                return Dismissible(
-                  key: Key(task.title),
-                  onDismissed: (_) {
-                    setState(() {
-                      _tasks.removeAt(index);
-                    });
-                  },
-                  background: Container(color: Colors.red),
-                  child: ListTile(
-                    title: Text(task.title),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+      body: Center(
+        child: Text(message),
       ),
     );
   }
